73e5ac83020535219624cdf785bf709e4bb1acb9
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2024-06-22 11:47:38 -0600

    cc

--- main.js
+++ main.js
@@ -1,4 +1,55 @@
-import {recipes} from './recipes.mjs';
+const recipes = [
+    
+    {
+        title: "Chicken Curry",
+        description: "A spicy and flavorful curry...",
+        image: "images/chicken-curry.webp",
+        rating: 5
+    },
+    {
+        title: "Apple Crisp",
+        description: "This apple crisp recipe is a simple yet delicious fall dessert that's great served warm with vanilla ice cream.",
+        image: "images/apple-crisp.jpg",
+        rating: 4
+    },
+    {
+        title: "Black Beans and Rice",
+        description: "A simple and healthy dish...",
+        image: "images/black-beans-and-rice.jpg",
+        rating: 4
+    },
+    {
+        title: "Chocolate Chip Cookies",
+        description: "Classic chocolate chip cookies...",
+        image: "images/chocolate-chip-cookies.jpg",
+        rating: 5
+    },
+    {
+        title: "Escalopes de Poulet à la Crème",
+        description: "A creamy chicken dish...",
+        image: "images/escalopes-de-poulet-a-la-creme.webp",
+        rating: 5
+    },
+    {
+        title: "German Gooseberry Cake",
+        description: "A sweet and tangy cake...",
+        image: "images/german-gooseberry-cake.jpg",
+        rating: 4
+    },
+    {
+        title: "Roasted Potatoes",
+        description: "Crispy and delicious roasted potatoes...",
+        image: "images/roasted-potatoes.webp",
+        rating: 5
+    },
+    {
+        title: "Sweet Potato Waffle",
+        description: "A delightful sweet potato waffle...",
+        image: "images/sweet-potato_waffle-md.jpg",
+        rating: 4
+    }
+];
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const recipesContainer = document.querySelector('.recipes');
